6dc315c941fbf7b4fd441e9b54eb4856d460fa95
lixx11 <lxx2011011580@gmail.com>
2017-01-29 11:31:01 +0800

    fix bug

--- diffraction_simulation.py
+++ diffraction_simulation.py
@@ -150,8 +150,6 @@ if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-    if rank == 0:
-
 
     if rank == 0:
         if os.path.isdir(output_dir):
