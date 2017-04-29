4172e65a78553e009555fa23665a8a74e9459db0
lixx11 <lxx2011011580@gmail.com>
2017-04-29 21:14:18 +0800

    recover expected restraint

--- evaluate_size.py
+++ evaluate_size.py
@@ -11,6 +11,9 @@ Options:
     --exp-spacing-std=exp_spacing_std   Max standard derivation of experimental spacing [default: 1.0].
     --sim-spacing-std=sim_spacing_std   Max standard derivation of simulated spacing [default: 1.0].
     --diff-angle=diff_angle             Max angle difference between simulated and experimental pattern [default: 3].
+    --expected-size=expected_size       Expected particle size in nanometer[default: 52].
+    --upper-limit=upper_limit           Upper limit ratio of particle size against expected [default: 1.5].
+    --lower-limit=lower_limit           Lower limit ratio of particle size against expected [default: 0.5].
     --output=output_dir                 Output filename [default: size.txt].
 '''
 
@@ -25,10 +28,13 @@ if __name__ == '__main__':
     argv = docopt(__doc__)
     size_files = argv['<size_files>']
     min_pcc = float(argv['--pcc'])
+    expected_size = float(argv['--expected-size']) * 1E-9
     min_max_intensity = float(argv['--max-intensity'])
     max_exp_spacing_std = float(argv['--exp-spacing-std'])
     max_sim_spacing_size = float(argv['--sim-spacing-std'])
     diff_angle = float(argv['--diff-angle'])
+    upper_limit = float(argv['--upper-limit'])
+    lower_limit = float(argv['--lower-limit'])
     output = argv['--output']
 
     # do evaluation and collection
@@ -47,7 +53,9 @@ if __name__ == '__main__':
                 and exp_max_intensities[i] > min_max_intensity \
                 and exp_spacing_stds[i] < max_exp_spacing_std \
                 and (exp_max_angles[i] - sim_max_angles[i]) < diff_angle \
-                and sim_spacing_stds[i] < max_sim_spacing_size:
+                and sim_spacing_stds[i] < max_sim_spacing_size \
+                and exp_particle_sizes[i] < expected_size * upper_limit \
+                and exp_particle_sizes[i] > expected_size * lower_limit:
                 valid_sizes.append(exp_particle_sizes[i])
     valid_sizes = np.asarray(valid_sizes)
     np.savetxt(output, valid_sizes)
